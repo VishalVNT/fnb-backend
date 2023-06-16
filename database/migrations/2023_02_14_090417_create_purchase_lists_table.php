bf1b2361b1f969d129d3960da5fa4da409f350fb
rohitvishin <rohitvishwakarma65@gmail.com>
2023-06-16 16:39:49 +0530

    purchase list schema

---
+++ database/migrations/2023_02_14_090417_create_purchase_lists_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_lists', function (Blueprint $table) {
+            $table->id();
+            $table->integer('company_id');
+            $table->integer('invoice_no');
+            $table->integer('court_fees');
+            $table->integer('tcs');
+            $table->integer('total_amount');
+            $table->integer('batch_no');
+            $table->integer('discount')->nullable();
+            $table->integer('vat')->nullable();
+            $table->integer('vendor_id')->nullable();
+            $table->integer('total_item')->nullable();
+            $table->date('invoice_date')->nullable();
+            $table->integer('isInvoice')->nullable();
+            $table->integer('status')->nullable()->default(1)->comment('1:active,0:inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
